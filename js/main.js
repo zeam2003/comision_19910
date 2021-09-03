5d79a06551efb4ef7b35a8484d2a438a34adb44b
zeam2003 <zeam2003@outlook.com>
2021-09-02 21:06:26 -0300

    primer commit clase 2

--- js/main.js
+++ js/main.js
@@ -1,42 +1,7 @@
-let nuestraEdad = prompt('Ingrese su edad');
-let año = 2021;
 
+let estaLloviendo = false;
 
-console.log(parseInt(nuestraEdad));
-alert(año);
-
-
-// alert(nuestraEdad);
-
-
-
-
-
-
-
-
-// let nombre = prompt('Ingrese su nombre: ');
-/* let nombre = 'Emiliano';
-let apellido = 'Kosh';
-
-console.log(nombre, apellido);
- */
-
-
-// var usuario = 'Dante'; // Esto no se tiene que usar
-/* 
-let usuario = 'Dante';
-
-
-const usuario2 = 'Dante';
- */
-
-/* let edadJuan = 20;
-
-let edadMatias = 18;
-
-let sumaEdades = edadJuan + edadMatias; */
-
-
-// console.log(sumaEdades);
+if (estaLloviendo) {
+    alert('No esta lloviendo');
+}
 
