15b34db5ac9f9ceba86d4d3f5b690989e9d32b0d
zeam2003 <zeam2003@outlook.com>
2021-09-07 20:40:41 -0300

    entregable 2

---
+++ Ejercicios/index.js
@@ -0,0 +1,59 @@
+//Clase 2
+
+
+
+/* 
+let nombreIngresado = prompt('Ingresar Nombre');
+let apellidoIngresado = prompt('Ingresar apellido');
+
+if (nombreIngresado != '' || apellidoIngresado != '') {
+    alert('Nombre: ' + nombreIngresado + '\nApellido: ' + apellidoIngresado);
+} else {
+    alert('Error: Ingresar nombre y apellido');
+} */
+/* 
+if (nombreIngresado != '' && apellidoIngresado != ''){
+    alert('Nombre: ' + nombreIngresado + '\nApellido: ' + apellidoIngresado);
+} else {
+    alert('Error: Ingresar nombre y apellido');
+} */
+
+
+
+/* let edad = 18;
+let edadIngresada = prompt("Ingresa la edad: ");
+let edadingresadaParseada = parseInt(edadIngresada);
+
+if (edad === edadingresadaParseada) {
+    alert('Es igual');
+} else {
+    alert('No es igual');
+} */
+
+
+/* if (edad < 18) {
+    alert("Es menor de Edad");  
+} else if (edad == 18) {
+    alert("Tiene 18");
+} else {
+    alert("Es mayor de edad");
+} */
+
+
+/* let nombreUsuario = prompt('Nos dices tu nombre?');
+
+if (nombreUsuario == "") {
+    alert('No ingresaste nada');
+} else {
+    alert('Ingresastes ' + nombreUsuario);
+} */
+
+/* let precio = 100;
+
+if (precio < 20) {
+    console.log("Es barato");
+} else if (precio < 60) {
+    console.log("El precio es normal");
+} else {
+    console.log("Es caro");
+} */
--- js/main.js
+++ js/main.js
@@ -1,7 +1,24 @@
+let numero = prompt("Ingrese un Número");
 
-let estaLloviendo = false;
+if( numero > 1000 ) {
+    alert("El número es mayor a 1000");
+} else {
+    alert("El número no cumple la condición");
+}
 
-if (estaLloviendo) {
-    alert('No esta lloviendo');
+let textoIngresado = prompt("Ingrese un Texto");
+
+if ( textoIngresado == "Hola" ) {
+    console.log("Se ingreso Hola");
+} else {
+    console.log("No se ingreso la palabra esperada");
+}
+
+let numeroIngresado = prompt("Ingrese nuevamente un Número");
+
+if (numeroIngresado >= 10 && numeroIngresado <= 50 ){
+    alert("estas entre 10 y 50");
+} else {
+    alert("No estas entre el rango esperado");
 }
 
